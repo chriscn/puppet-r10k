469636ac93d3f3a5c3c7543fbf70a64e02035f5a
Christopher Nethercott <christopher.nethercott@york.ac.uk>
2025-06-12 15:52:20 +0100

    Add missing config type

--- types/webhook/config.pp
+++ types/webhook/config.pp
@@ -1,6 +1,7 @@
 # @summary webhook config type
 type R10k::Webhook::Config = Struct[{
-    server  => Optional[R10k::Webhook::Config::Server],
-    chatops => Optional[R10k::Webhook::Config::Chatops],
-    r10k    => Optional[R10k::Webhook::Config::R10k],
+    server   => Optional[R10k::Webhook::Config::Server],
+    chatops  => Optional[R10k::Webhook::Config::Chatops],
+    r10k     => Optional[R10k::Webhook::Config::R10k],
+    mappings => Optional[R10k::Webhook::Config::Mappings],
 }]
